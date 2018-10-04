6c98bfe80a1bb5cebeb8818146ed23f071d04b3a
gonza <gonzalo.muscolo@gmail.com>
2018-10-04 02:06:49 -0300

    hago funcion grilla

--- interfaz/js/main.js
+++ interfaz/js/main.js
@@ -23,3 +23,36 @@ $('#menosEntrada').click(function(){
     $('#cantidadEntradas').val(cantidadEntrada);
    }
 })
+
+$('#prueba').click(function(){
+hacerGrilla(5,15)
+ })
+
+function hacerGrilla(filas,columnas){
+    var grilla = '<table>'
+    var numeracion = 0;
+    for (i = 1; i <= filas; i++) {
+        grilla+='<tr>'
+        for(y=1;y<=columnas;y++){
+            numeracion++
+            grilla+='<td><div id="butaca'+numeracion+'" class="circle text-center" onclick=reservarButaca('+i+','+y+','+numeracion+')>'+numeracion+'</div></td>';
+        }
+        grilla+='</tr>'
+    }
+    grilla+='</table>'
+
+    $('#tablaAsientos').append(grilla);
+}
+
+
+function reservarButaca(fila,columna,id){4
+    //verifico si ya esta reservada
+    if(!$( "#butaca"+id ).is( ".rojo" )){
+        // verifico si ya fue seleccionda
+      if($( "#butaca"+id ).is( ".verde" )){
+        $('#butaca'+id).removeClass('verde');
+      }else{
+          $('#butaca'+id).addClass('verde');
+      }  
+    }
+}

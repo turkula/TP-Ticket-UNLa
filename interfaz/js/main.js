50feb89bbb77e82483b91647dae85d7d53f2eb05
gonzabt3 <fm_gonza@hotmail.com>
2018-10-04 16:59:53 -0300

    sigo interfaz

--- interfaz/js/main.js
+++ interfaz/js/main.js
@@ -1,3 +1,7 @@
+var arrayButacasCine = []
+var arrayButacasOcupadas = [1,55,10,4]
+
+
 $('#btnLogin').click(function(){
 
     var data = {
@@ -25,17 +29,28 @@ $('#menosEntrada').click(function(){
 })
 
 $('#prueba').click(function(){
-hacerGrilla(5,15)
+hacerGrilla(9,15,arrayButacasOcupadas)
  })
 
-function hacerGrilla(filas,columnas){
+
+function hacerGrilla(filas,columnas,arrayButacasOcupadas){
     var grilla = '<table>'
     var numeracion = 0;
     for (i = 1; i <= filas; i++) {
         grilla+='<tr>'
         for(y=1;y<=columnas;y++){
             numeracion++
-            grilla+='<td><div id="butaca'+numeracion+'" class="circle text-center" onclick=reservarButaca('+i+','+y+','+numeracion+')>'+numeracion+'</div></td>';
+
+            // PINTO BUTACAS OCUPADAS DE ROJO
+            color="";
+            arrayButacasOcupadas.forEach(function(element){
+                console.log(element);
+                if(numeracion==element){
+                    color="rojo";
+                }
+            })
+
+            grilla+='<td><div id="butaca'+numeracion+'" class="circle text-center '+color+'" onclick=reservarButaca('+i+','+y+','+numeracion+')>'+numeracion+'</div></td>';
         }
         grilla+='</tr>'
     }
@@ -48,11 +63,30 @@ function hacerGrilla(filas,columnas){
 function reservarButaca(fila,columna,id){4
     //verifico si ya esta reservada
     if(!$( "#butaca"+id ).is( ".rojo" )){
+
         // verifico si ya fue seleccionda
       if($( "#butaca"+id ).is( ".verde" )){
         $('#butaca'+id).removeClass('verde');
+
+        // saco la butaca del array
+        arrayButacasCine.forEach(function(butaca,index) {
+            if(butaca.id==id){
+                arrayButacasCine.splice(index, 1);
+            }
+          })
+
       }else{
           $('#butaca'+id).addClass('verde');
+
+          //creo objeto butaca
+          butaca={
+              fila:fila,
+              columna:columna,
+              id:id
+          }
+
+          //lo meto en el array de cine
+          arrayButacasCine.push(butaca);
       }  
     }
 }

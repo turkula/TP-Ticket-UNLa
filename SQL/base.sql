b7730a67da94954952069dc144cdbc126b147e93
turkula <claudioturkula@hotmail.com>
2018-10-12 12:17:29 -0300

    Update SQL/base.sql

--- SQL/base.sql
+++ SQL/base.sql
@@ -24,13 +24,14 @@ DROP TABLE IF EXISTS `administrador`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `administrador` (
   `idAdministrador` int(11) NOT NULL,
-  `tipoadministrador_idTipoAdministrador` int(11) NOT NULL,
+  `idTipoAdministrador` int(11) DEFAULT NULL,
   `idAuditorio` int(11) DEFAULT NULL,
   PRIMARY KEY (`idAdministrador`),
-  KEY `fk_administrador_tipoadministrador1_idx` (`tipoadministrador_idTipoAdministrador`),
   KEY `administrador-auditorio_idx` (`idAuditorio`),
+  KEY `administrador-tipo_idx` (`idTipoAdministrador`),
   CONSTRAINT `administrador-auditorio` FOREIGN KEY (`idAuditorio`) REFERENCES `auditorio` (`idAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_administrador_tipoadministrador1` FOREIGN KEY (`tipoadministrador_idTipoAdministrador`) REFERENCES `tipoadministrador` (`idTipoAdministrador`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `administrador-tipo` FOREIGN KEY (`idTipoAdministrador`) REFERENCES `tipoadministrador` (`idTipoAdministrador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `administrador-usuario` FOREIGN KEY (`idAdministrador`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -51,12 +52,12 @@ DROP TABLE IF EXISTS `auditorio`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auditorio` (
-  `idAuditorio` int(11) NOT NULL,
+  `idAuditorio` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   `idTipoAuditorio` int(11) DEFAULT NULL,
   PRIMARY KEY (`idAuditorio`),
-  KEY `auditorio-tipoauditorio_idx` (`idTipoAuditorio`),
-  CONSTRAINT `auditorio-tipoauditorio` FOREIGN KEY (`idTipoAuditorio`) REFERENCES `tipoauditorio` (`idTipoAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `auditorio-tipo_idx` (`idTipoAuditorio`),
+  CONSTRAINT `auditorio-tipo` FOREIGN KEY (`idTipoAuditorio`) REFERENCES `tipoauditorio` (`idTipoAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -80,10 +81,10 @@ CREATE TABLE `butaca` (
   `idButaca` int(11) NOT NULL AUTO_INCREMENT,
   `fila` int(11) DEFAULT NULL,
   `columna` int(11) DEFAULT NULL,
-  `idSector` int(11) DEFAULT NULL,
+  `idNumerada` int(11) DEFAULT NULL,
   PRIMARY KEY (`idButaca`),
-  KEY `fkSector_idx` (`idSector`),
-  CONSTRAINT `fkSector` FOREIGN KEY (`idSector`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `butaca-numerada_idx` (`idNumerada`),
+  CONSTRAINT `butaca-numerada` FOREIGN KEY (`idNumerada`) REFERENCES `numerada` (`idNumerada`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -105,12 +106,13 @@ DROP TABLE IF EXISTS `cliente`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cliente` (
   `idCliente` int(11) NOT NULL,
-  `tipocliente_idTipoCliente` int(11) NOT NULL,
-  `fechaNacimiento` datetime DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `idTipoCliente` int(11) DEFAULT NULL,
   PRIMARY KEY (`idCliente`),
-  KEY `fk_cliente_tipocliente1_idx` (`tipocliente_idTipoCliente`),
-  CONSTRAINT `fk_cliente_tipocliente1` FOREIGN KEY (`tipocliente_idTipoCliente`) REFERENCES `tipocliente` (`idTipoCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `cliente-tipo_idx` (`idTipoCliente`),
+  CONSTRAINT `cliente-tipo` FOREIGN KEY (`idTipoCliente`) REFERENCES `tipocliente` (`idTipoCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `cliente-usuario` FOREIGN KEY (`idCliente`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,10 +132,11 @@ DROP TABLE IF EXISTS `codigo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `codigo` (
-  `idCodigo` int(11) NOT NULL AUTO_INCREMENT,
+  `idCodigo` int(11) NOT NULL,
   `codigo` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idCodigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`idCodigo`),
+  CONSTRAINT `codigo-descuento` FOREIGN KEY (`idCodigo`) REFERENCES `descuento` (`idDescuento`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,13 +159,14 @@ DROP TABLE IF EXISTS `descuento`;
 CREATE TABLE `descuento` (
   `idDescuento` int(11) NOT NULL AUTO_INCREMENT,
   `porcentaje` int(11) DEFAULT NULL,
-  `usado` tinyint(4) DEFAULT NULL,
+  `usado` bit(1) DEFAULT NULL,
   `idFuncion` int(11) DEFAULT NULL,
+  `idSector` int(11) DEFAULT NULL,
   PRIMARY KEY (`idDescuento`),
   KEY `descuento-funcion_idx` (`idFuncion`),
-  CONSTRAINT `descuento-codigo` FOREIGN KEY (`idDescuento`) REFERENCES `codigo` (`idCodigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `descuento-dia` FOREIGN KEY (`idDescuento`) REFERENCES `dia` (`idDia`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `descuento-funcion` FOREIGN KEY (`idFuncion`) REFERENCES `funcion` (`idFuncion`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `descuento-sector_idx` (`idSector`),
+  CONSTRAINT `descuento-funcion` FOREIGN KEY (`idFuncion`) REFERENCES `funcion` (`idFuncion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `descuento-sector` FOREIGN KEY (`idSector`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -172,7 +176,7 @@ CREATE TABLE `descuento` (
 
 LOCK TABLES `descuento` WRITE;
 /*!40000 ALTER TABLE `descuento` DISABLE KEYS */;
-INSERT INTO `descuento` VALUES (1,5,0,NULL),(2,5,0,NULL),(3,5,0,NULL),(4,5,0,NULL),(5,5,0,NULL),(6,5,0,NULL),(7,5,0,NULL);
+INSERT INTO `descuento` VALUES (1,5,'\0',NULL,NULL),(2,5,'\0',NULL,NULL),(3,5,'\0',NULL,NULL),(4,5,'\0',NULL,NULL),(5,5,'\0',NULL,NULL),(6,5,'\0',NULL,NULL),(7,5,'\0',NULL,NULL);
 /*!40000 ALTER TABLE `descuento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -184,9 +188,10 @@ DROP TABLE IF EXISTS `dia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dia` (
-  `idDia` int(11) NOT NULL AUTO_INCREMENT,
+  `idDia` int(11) NOT NULL,
   `dia` date DEFAULT NULL,
-  PRIMARY KEY (`idDia`)
+  PRIMARY KEY (`idDia`),
+  CONSTRAINT `dia-descuento` FOREIGN KEY (`idDia`) REFERENCES `descuento` (`idDescuento`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -207,15 +212,15 @@ DROP TABLE IF EXISTS `evento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `evento` (
-  `idEvento` int(11) NOT NULL,
+  `idEvento` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   `idTipoEvento` int(11) DEFAULT NULL,
   `idAuditorio` int(11) DEFAULT NULL,
   PRIMARY KEY (`idEvento`),
   KEY `evento-auditorio_idx` (`idAuditorio`),
-  KEY `evento-tipoevento_idx` (`idTipoEvento`),
+  KEY `evento-tipo_idx` (`idTipoEvento`),
   CONSTRAINT `evento-auditorio` FOREIGN KEY (`idAuditorio`) REFERENCES `auditorio` (`idAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `evento-tipoevento` FOREIGN KEY (`idTipoEvento`) REFERENCES `tipoevento` (`idTipoevento`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `evento-tipo` FOREIGN KEY (`idTipoEvento`) REFERENCES `tipoevento` (`idTipoevento`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -236,7 +241,7 @@ DROP TABLE IF EXISTS `funcion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `funcion` (
-  `idFuncion` int(11) NOT NULL,
+  `idFuncion` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(45) DEFAULT NULL,
   `fechaHora` varchar(45) DEFAULT NULL,
   `idEvento` int(11) DEFAULT NULL,
@@ -263,11 +268,10 @@ DROP TABLE IF EXISTS `numerada`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `numerada` (
-  `idNumerada` int(11) NOT NULL AUTO_INCREMENT,
+  `idNumerada` int(11) NOT NULL,
   `idButaca` int(11) DEFAULT NULL,
   PRIMARY KEY (`idNumerada`),
-  KEY `numerada-butaca_idx` (`idButaca`),
-  CONSTRAINT `numerada-butaca` FOREIGN KEY (`idButaca`) REFERENCES `butaca` (`idButaca`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `numerada-sector` FOREIGN KEY (`idNumerada`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -288,14 +292,14 @@ DROP TABLE IF EXISTS `persona`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `persona` (
-  `idPersona` int(11) NOT NULL,
-  `usuario_idUsuario` int(11) NOT NULL,
+  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) DEFAULT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `apellido` varchar(45) DEFAULT NULL,
   `dni` int(11) DEFAULT NULL,
   PRIMARY KEY (`idPersona`),
-  KEY `fk_persona_usuario_idx` (`usuario_idUsuario`),
-  CONSTRAINT `fk_persona_usuario` FOREIGN KEY (`usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `persona usuario_idx` (`idUsuario`),
+  CONSTRAINT `persona usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -316,9 +320,10 @@ DROP TABLE IF EXISTS `popular`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `popular` (
-  `idPopular` int(11) NOT NULL AUTO_INCREMENT,
+  `idPopular` int(11) NOT NULL,
   `cantidadMaxima` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idPopular`)
+  PRIMARY KEY (`idPopular`),
+  CONSTRAINT `popular-sector` FOREIGN KEY (`idPopular`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -339,7 +344,7 @@ DROP TABLE IF EXISTS `reserva`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reserva` (
-  `idreserva` int(11) NOT NULL,
+  `idreserva` int(11) NOT NULL AUTO_INCREMENT,
   `idTicket` int(11) DEFAULT NULL,
   PRIMARY KEY (`idreserva`),
   KEY `reserva-ticket_idx` (`idTicket`),
@@ -364,12 +369,12 @@ DROP TABLE IF EXISTS `sector`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sector` (
-  `idSector` int(11) NOT NULL,
+  `idSector` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(45) DEFAULT NULL,
+  `idAuditorio` int(11) DEFAULT NULL,
   PRIMARY KEY (`idSector`),
-  CONSTRAINT `sector-auditorio` FOREIGN KEY (`idSector`) REFERENCES `auditorio` (`idAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `sector-numerada` FOREIGN KEY (`idSector`) REFERENCES `numerada` (`idNumerada`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `sector-popular` FOREIGN KEY (`idSector`) REFERENCES `popular` (`idPopular`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `sector-auditorio_idx` (`idAuditorio`),
+  CONSTRAINT `sector-auditorio` FOREIGN KEY (`idAuditorio`) REFERENCES `auditorio` (`idAuditorio`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -396,8 +401,8 @@ CREATE TABLE `tarifa` (
   `idFuncion` int(11) DEFAULT NULL,
   PRIMARY KEY (`idTarifa`),
   KEY `tarifa-funcion_idx` (`idTarifa`,`idFuncion`),
-  KEY `tarifa-funcion_idx1` (`idFuncion`),
   KEY `tarifa-sector_idx` (`idSector`),
+  KEY `tarifa-funcion_idx1` (`idFuncion`),
   CONSTRAINT `tarifa-funcion` FOREIGN KEY (`idFuncion`) REFERENCES `funcion` (`idFuncion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `tarifa-sector` FOREIGN KEY (`idSector`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -420,7 +425,7 @@ DROP TABLE IF EXISTS `ticket`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ticket` (
-  `idTicket` int(11) NOT NULL,
+  `idTicket` int(11) NOT NULL AUTO_INCREMENT,
   `idCliente` int(11) DEFAULT NULL,
   `idFuncion` int(11) DEFAULT NULL,
   `idSector` int(11) DEFAULT NULL,
@@ -454,7 +459,7 @@ DROP TABLE IF EXISTS `tipoadministrador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tipoadministrador` (
-  `idTipoAdministrador` int(11) NOT NULL,
+  `idTipoAdministrador` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idTipoAdministrador`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -501,7 +506,7 @@ DROP TABLE IF EXISTS `tipocliente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tipocliente` (
-  `idTipoCliente` int(11) NOT NULL,
+  `idTipoCliente` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idTipoCliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -548,12 +553,10 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombreUsuario` varchar(45) DEFAULT NULL,
   `clave` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idUsuario`),
-  CONSTRAINT `usuario-administrador` FOREIGN KEY (`idUsuario`) REFERENCES `administrador` (`idAdministrador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `usuario-cliente` FOREIGN KEY (`idUsuario`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -575,4 +578,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-12  1:24:12
+-- Dump completed on 2018-10-12 12:13:51
